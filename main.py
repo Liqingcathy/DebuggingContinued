90d5d2af632364fb45c271830cafd861ec8387fe
Liqingcathy <cathy.glc@gmail.com>
2022-03-21 13:25:13 -0700

    debug comlished

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from gameoflife import width, height, stage, print_stage, count_neighbors
-
+import copy
 def init_stage(stage):
     for v_pos in range(0, height):
       for h_pos in range(0, width):
@@ -15,9 +15,10 @@ def init_stage(stage):
 # 4. Any dead cell with 3 neighbors comes alive
 
 def one_generation(stage):
+    stage_deep_copy = copy.deepcopy(stage)
     for v_pos in range(len(stage)):
-        for h_pos in range(len(v_pos)):
-            neighbors = count_neighbors(stage, v_pos, h_pos)
+        for h_pos in range(len(stage[v_pos])):
+            neighbors = count_neighbors(stage_deep_copy, v_pos, h_pos)
             if not stage[v_pos][h_pos] and neighbors == 3:
                 stage[v_pos][h_pos] = True
             elif stage[v_pos][h_pos] and neighbors < 2:
